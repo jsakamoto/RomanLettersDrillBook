19fa0ae926893356065960f9136de1eb2f8dbf64
jsakamoto <junichi.jp@gmail.com>
2014-09-04 21:29:31 +0900

    Implement drawing feature.

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
-﻿var RomanLettersDrillBook;
+﻿/// <reference path="typings/jquery/jquery.d.ts" />
+var RomanLettersDrillBook;
 (function (RomanLettersDrillBook) {
-    alert('Hello World.');
+    var Point = (function () {
+        function Point() {
+        }
+        return Point;
+    })();
+
+    var DrawableCanvas = (function () {
+        function DrawableCanvas(canvasElement) {
+            var _this = this;
+            this.lastPoint = null;
+            this.context = canvasElement.getContext('2d');
+            this.context.lineJoin = 'round';
+            this.context.lineWidth = 4;
+
+            var convertToPoint = function (e) {
+                return { x: e.offsetX, y: e.offsetY };
+            };
+
+            var drawStroke = function (e, isEnd) {
+                if (_this.lastPoint == null)
+                    return;
+                var curPoint = convertToPoint(e);
+                _this.context.beginPath();
+                _this.context.moveTo(_this.lastPoint.x, _this.lastPoint.y);
+                _this.context.lineTo(curPoint.x, curPoint.y);
+                _this.context.closePath();
+                _this.context.stroke();
+                _this.lastPoint = isEnd ? null : curPoint;
+            };
+
+            var $canvas = $(canvasElement);
+            $canvas.mousedown(function (e) {
+                return _this.lastPoint = convertToPoint(e);
+            }).mouseup(function (e) {
+                return _this.lastPoint = null;
+            }).mousemove(function (e) {
+                return drawStroke(e, false);
+            }).mouseleave(function (e) {
+                return drawStroke(e, true);
+            }).mouseenter(function (e) {
+                return drawStroke(e, false);
+            });
+        }
+        return DrawableCanvas;
+    })();
+
+    $('.canvas-cell').each(function (n, e) {
+        return new DrawableCanvas(e);
+    });
 })(RomanLettersDrillBook || (RomanLettersDrillBook = {}));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,3 +1,43 @@
-﻿module RomanLettersDrillBook {
-    alert('Hello World.');
+﻿/// <reference path="typings/jquery/jquery.d.ts" />
+module RomanLettersDrillBook {
+
+    class Point {
+        x: number;
+        y: number;
+    }
+
+    class DrawableCanvas {
+        lastPoint: Point;
+        context: CanvasRenderingContext2D;
+
+        constructor(canvasElement: any) {
+            this.lastPoint = null;
+            this.context = canvasElement.getContext('2d');
+            this.context.lineJoin = 'round';
+            this.context.lineWidth = 4;
+
+            var convertToPoint = (e: JQueryMouseEventObject) => { return { x: e.offsetX, y: e.offsetY } };
+
+            var drawStroke = (e: JQueryMouseEventObject, isEnd: boolean) => {
+                if (this.lastPoint == null) return;
+                var curPoint = convertToPoint(e);
+                this.context.beginPath();
+                this.context.moveTo(this.lastPoint.x, this.lastPoint.y);
+                this.context.lineTo(curPoint.x, curPoint.y);
+                this.context.closePath();
+                this.context.stroke();
+                this.lastPoint = isEnd ? null : curPoint;
+            };
+
+            var $canvas = $(canvasElement);
+            $canvas
+                .mousedown(e => this.lastPoint = convertToPoint(e))
+                .mouseup(e => this.lastPoint = null)
+                .mousemove(e => drawStroke(e, false))
+                .mouseleave(e => drawStroke(e, true))
+                .mouseenter(e => drawStroke(e, false));
+        }
+    }
+
+    $('.canvas-cell').each((n, e) => new DrawableCanvas(e));
 } 
